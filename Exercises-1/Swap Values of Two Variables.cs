7fbc83ed8eee453d37c439d002a9b2bef62c7084
phanthanhthuy11100-droid <phanthanhthuy11100@gmail.com>
2026-04-23 23:16:20 +0700

    Add files via upload

---
+++ Exercises-1/Swap Values of Two Variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Swapvaluesoftwovariables
+{
+    class SwapProgram
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before swapping: a = " + a + ", b = " + b);
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After swapping: a = " + a + ", b = " + b);
+        }
+    }
+}
