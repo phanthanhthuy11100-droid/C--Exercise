6febad1192631f8b6abfd420ede088d01843190a
phanthanhthuy11100-droid <phanthanhthuy11100@gmail.com>
2026-04-23 23:22:46 +0700

    Add files via upload

---
+++ Exercises-1/Calculate Area of Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace caculateareaofcircle
+{
+    class AreaOfCircleProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tinh dien tich hinh tron");
+            Console.Write("Nhap ban kinh: ");
+            double r = double.Parse(Console.ReadLine());
+            double PI = Math.PI;
+            double area = PI * r * r;
+            Console.WriteLine("Dien tich hinh tron la: " + area);
+        }
+    }
+}
