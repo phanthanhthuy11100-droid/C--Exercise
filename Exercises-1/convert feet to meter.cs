5cbf83b93d48eb8e64c2679fdd3008f20174e55b
phanthanhthuy11100-droid <phanthanhthuy11100@gmail.com>
2026-04-23 23:18:50 +0700

    Add files via upload

---
+++ Exercises-1/convert feet to meter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Convertfeettometer {
+    class ConvertProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chuyen doi feet sang meter");
+            Console.Write("Nhap so feet: ");
+            double feet = double.Parse(Console.ReadLine());
+            double meter = feet * 0.3048;
+            Console.WriteLine("So met la: " + meter);
+        }
+    }
+}
