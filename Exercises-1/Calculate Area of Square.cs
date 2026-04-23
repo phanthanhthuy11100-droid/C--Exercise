05869838a81408346a83145673fe7da08dd04bdc
phanthanhthuy11100-droid <phanthanhthuy11100@gmail.com>
2026-04-23 23:23:00 +0700

    Add files via upload

---
+++ Exercises-1/Calculate Area of Square.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace caculateareaofsquare {
+    class AreaOfSquareProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tinh dien tich hinh vuong");
+            Console.Write("Nhap do dai canh: ");
+            double a = double.Parse(Console.ReadLine());
+            double area = a * a;
+            Console.WriteLine("Dien tich hinh vuong la: " + area);
+        }
+    }
+}
