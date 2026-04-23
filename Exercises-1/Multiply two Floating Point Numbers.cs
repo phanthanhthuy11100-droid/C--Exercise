f6dc9feed3eaa08ff0590cdbb3229b48078a5bb0
phanthanhthuy11100-droid <phanthanhthuy11100@gmail.com>
2026-04-23 23:18:12 +0700

    Add files via upload

---
+++ Exercises-1/Multiply two Floating Point Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Multiplytwofloatingpointnumbers
+{
+    class MultiplyProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so thuc a:");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so thuc b:");
+            float num2 = float.Parse(Console.ReadLine());
+            float result = num1 * num2;
+            Console.WriteLine("Tich cua " + num1 + " va " + num2 + " la: " + result);
+        }
+    }
+}
