23d3a97f5b8388c9606f47a8c9133ea0b001d7f4
phanthanhthuy11100-droid <phanthanhthuy11100@gmail.com>
2026-04-23 23:21:26 +0700

    Add files via upload

---
+++ Exercises-1/Print ASCII Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PrintASCIIvalue {
+    class ASCIIProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap mot ky tu: ");
+            char c = Console.ReadKey().KeyChar;
+            int asciiValue = (int)c;
+            Console.WriteLine("Ma ASCII cua ky tu la: " + asciiValue);
+        }
+    }
+}
