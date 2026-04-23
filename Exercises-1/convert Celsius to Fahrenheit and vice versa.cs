1a5be74aaec637b1ea21ce68510291dca9ea7ed0
phanthanhthuy11100-droid <phanthanhthuy11100@gmail.com>
2026-04-23 23:19:28 +0700

    Add files via upload

---
+++ Exercises-1/convert Celsius to Fahrenheit and vice versa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace convertcelsiustofahrenheitandviceversa
+{
+    class ConvertcProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chuyen doi do C sang do F va nguoc lai");
+            Console.Write("Nhap do C: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Do F tuong ung la: " + (c * 9 / 5 + 32));
+            Console.Write("Nhap do F: ");
+            double f = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Do C tuong ung la: " + ((f - 32) * 5 / 9));
+        }
+    }
+}
