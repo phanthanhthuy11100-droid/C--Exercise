f61732d1a256dd3d77710b2b2a7350b484639585
phanthanhthuy11100-droid <phanthanhthuy11100@gmail.com>
2026-04-23 23:20:54 +0700

    Add files via upload

---
+++ Exercises-1/find the Size of data types.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace findthesizeofdatatypes {
+    class SizeOfProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int: " + sizeof(int) + " bytes");
+            Console.WriteLine("Size of float: " + sizeof(float) + " bytes");
+            Console.WriteLine("Size of double: " + sizeof(double) + " bytes");
+            Console.WriteLine("Size of char: " + sizeof(char) + " bytes");
+            Console.WriteLine("Size of bool: " + sizeof(bool) + " bytes");
+        }
+    }
+}
