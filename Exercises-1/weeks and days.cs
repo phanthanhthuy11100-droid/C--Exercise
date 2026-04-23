46f08aae6c228836e7c99d6b455155b9d4b14696
phanthanhthuy11100-droid <phanthanhthuy11100@gmail.com>
2026-04-23 23:23:18 +0700

    Add files via upload

---
+++ Exercises-1/weeks and days.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace convertdaystoyearsweeksanddays
+{
+    class ConvertdayProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so ngay: ");
+            int days = int.Parse(Console.ReadLine());
+            int years = days / 365;
+            int remainingDays = days % 365;
+            int weeks = remainingDays / 7;
+            remainingDays = remainingDays % 7;
+            Console.WriteLine(days + " ngay bang " + years + " nam, " + weeks + " tuan va " + remainingDays + " ngay.");
+        }
+    }
+}
