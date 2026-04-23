5a5969051da561ca666ede1f1028e62a1b7cd4b6
phanthanhthuy11100-droid <phanthanhthuy11100@gmail.com>
2026-04-23 23:14:22 +0700

    Add files via upload

---
+++ Exercises-1/Add  Sum Two Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Sumtwonumber
+{
+    class SumProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("So thu nhat:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("So thu hai:");
+            int b = int.Parse(Console.ReadLine());
+            int result = a + b;
+            Console.WriteLine("Tong cua " + a + " va " + b + " la: " + result);
+        }
+    }
+}
